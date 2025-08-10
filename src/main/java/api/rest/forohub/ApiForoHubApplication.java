808e1ecd6750ec271902b98848ed9141b5314e13
ericks97 <erickaljndrr@gmail.com>
2025-08-10 09:35:56 -0400

    firstcommit

---
+++ src/main/java/api/rest/forohub/ApiForoHubApplication.java
@@ -0,0 +1,13 @@
+package api.rest.forohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForoHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/rest/forohub/ApiForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package api.rest.forohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
